e0cc95f788eb254ab1943cd8614a7cf5ac274a65
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2011-05-31 14:23:50 -0300

    Adicinados os timeouts.
    Falta tratar todos os tipos de erros que podem ocorrer.

--- cliente.py
+++ cliente.py
@@ -19,13 +19,25 @@ class Cliente(object):
     def log(self,msg,data=None):
 
         ''' diferentes niveis de log:
-            1 - inicio
-            2 - recebimento de mensagem
-            3 - envio de mensagem
+            1 - Inicio da execucao do cliente
+            2 - Tentativa de conexao
+            3 - Envio de dados
+            4 - Recebimento de dados
+            5 - Estouro de timeout na conexao
+            6 - Estouro de timeout no envio
+            7 - Estouro de timeout no recebimento
+            8 - Conexao efetuada
+            9 - Desligando cliente
         '''
-        text1 = "\nIniciando "+self.ME+"(em modo cliente): "+datetime.now().ctime()+"\n"
-        text2 = self.ME+" (cliente) diz: recebi "+str(data)+" em: "+datetime.now().ctime()+"\n"
-        text3 = self.ME+" (cliente) diz: enviei "+str(data)+" em: "+datetime.now().ctime()+"\n"
+        text1 = "\nIniciando "+self.ME+" em modo cliente: "+datetime.now().ctime()+"\n"
+        text2 = self.ME+" tentando conexao com "+self.HOST+": "+datetime.now().ctime()+"\n"
+        text3 = self.ME+" diz: recebi "+str(data)+" de "+self.HOST+" em: "+datetime.now().ctime()+"\n"
+        text4 = self.ME+" diz: enviei "+str(data)+" para "+self.HOST+" em: "+datetime.now().ctime()+"\n"
+        text5 = self.ME+" diz: estouro de timeout ao conectar-se com "+self.HOST+": "+datetime.now().ctime()+"\n"
+        text6 = self.ME+" diz: estouro de timeout ao enviar "+str(data)+" para "+self.HOST+": "+datetime.now().ctime()+"\n"
+        text7 = self.ME+" diz: estouro de timeout ao enviar "+str(data)+" para "+self.HOST+": "+datetime.now().ctime()+"\n"
+        text8 = self.ME+" diz: conectei-me a "+self.HOST+" em: "+datetime.now().ctime()+"\n"
+        text9 = self.ME+" diz: desligando em: "+datetime.now().ctime()+" flws \o\n"
 
         if msg == 1:
             # Inicia o log com a marcacao de tempo
@@ -36,36 +48,102 @@ class Cliente(object):
             self.logFile.write(text2)
 
         elif msg == 3:
-            # Envio de dados:
+            # Recebimento de dados:
             self.logFile.write(text3)
 
-        #else:
-        #    pass
+        elif msg == 4:
+            # Envio de dados:
+            self.logFile.write(text4)
+
+        elif msg == 5:
+            # Envio de dados:
+            self.logFile.write(text5)
+
+        elif msg == 6:
+            # Envio de dados:
+            self.logFile.write(text6)
+
+        elif msg == 7:
+            # Recebimento de dados:
+            self.logFile.write(text7)
+
+        elif msg == 8:
+            # Recebimento de dados:
+            self.logFile.write(text8)
+
+        elif msg == 9:
+            # Recebimento de dados:
+            self.logFile.write(text9)
 
 
     def start(self):
 
+        # Registra o inicio do cliente:
         self.log(1)
 
         # Abrindo um socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # Criando conexao:
-        self.sock.connect((self.HOST, self.PORT))
+        # Definindo um timeout para conectar:
+        self.sock.settimeout(5.0)
 
+        # Tenta se conectar 3x:
+#        for i in range(3):
+        try:
+            # Criando conexao:
+            print "Tentando conectar ao servidor..."
+            self.log(2)
+            self.sock.connect((self.HOST, self.PORT))
+            print "Conectado"
+            self.log(8)
+
+        except socket.timeout:
+            print "Estouro do timeout"
+            self.log(5)
+
+        # Reajustando o timeout, para falar com o servidor:
+        self.sock.settimeout(1.5)
+
+
+        # Conexao feita, eh hora de aguardar a entrada do usuario.
         while True:
 
             # Recebendo dados do usuario:
             data = raw_input("Digite a expressao matematica: ")
 
-            # Enviando dados e armazenando no log:
-            self.sock.send(data)
-            self.log(3,data)
+
+            # Tenta enviar no maximo 5 vezes
+            for i in range(5):
+
+                # Trata timeout
+                try:
+                    # Enviando dados e armazenando no log:
+                    self.sock.send(data)
+                    print "Enviei %s" % str(data)
+                    self.log(4,data)
+
+                except socket.timeout:
+                    print "Estouro de timeout no envio"
+                    self.log(6,data)
+
+                except:
+                    print "Nao conectado, desligando..."
+                    self.log(9)
+                    exit(1)
 
             # Recebendo dados do servidor e atualizando o log
-            # TIMEOUT AQUI
-            data = self.sock.recv(1024)
-            self.log(2,data)
-            print "Recebi %s" % str(data)
+            # Tenta receber no maximo 5 vezes
+            for i in range(5):
+
+                # Trata timeout
+                try:
+                    # Enviando dados e armazenando no log:
+                    data = self.sock.recv(1024)
+                    print "Recebi %s" % str(data)
+                    self.log(3,data)
+
+                except socket.timeout:
+                    print "Estouro de timeout no recebimento"
+                    self.log(7,data)
 
 
