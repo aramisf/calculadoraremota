637d9202d6978c0abef5bc0ce7262aea2255fb30
aramisf <ashf03@c3sl.ufpr.br>
2011-06-02 01:29:41 -0300

    Corrigido teste de timeout e mensagem de log.
    Ainda, melhorado o texto da resposta recebida pela outra ponta.

--- cliente.py
+++ cliente.py
@@ -6,6 +6,7 @@
 import socket, time
 from datetime import datetime
 
+
 class Cliente(object):
 
     def __init__(self,ME,l_hosts,l_ports,logFP):
@@ -26,6 +27,7 @@ class Cliente(object):
             5 - Estouro de timeout na conexao
             6 - Conexao efetuada
             7 - Desligando cliente
+            8 - Servidor aparenta estar desconectado
         '''
         text1 = "\nIniciando "+self.ME+" em modo cliente: "+datetime.now().ctime()+"\n"
         text2 = self.ME+" tentando conexao com "+self.HOST+": "+datetime.now().ctime()+"\n"
@@ -34,6 +36,7 @@ class Cliente(object):
         text5 = self.ME+" diz: estouro de timeout ao conectar-se com "+self.HOST+": "+datetime.now().ctime()+"\n"
         text6 = self.ME+" diz: conectei-me a "+self.HOST+" em: "+datetime.now().ctime()+"\n"
         text7 = self.ME+" diz: desligando em: "+datetime.now().ctime()+" flws \o\n"
+        text8 = self.ME+" diz: servidor aparenta estar desconectado: "+datetime.now().ctime()+"\n"
 
         if msg == 1:
             # Inicia o log com a marcacao de tempo
@@ -63,6 +66,10 @@ class Cliente(object):
             # Recebimento de dados:
             self.logFile.write(text7)
 
+        elif msg == 8:
+            # Envio de dados:
+            self.logFile.write(text8)
+
 
     def start(self):
 
@@ -72,54 +79,51 @@ class Cliente(object):
         # Abrindo um socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # Definindo um timeout para conectar:
-        self.sock.settimeout(5.)
-
         try:
             # Criando conexao:
             print "Tentando conectar ao servidor..."
             self.log(2)
             self.sock.connect((self.HOST, self.PORT))
-            print "Conectado"
-            self.log(6)
 
-        except socket.timeout:
-            print "Estouro do timeout"
-            self.log(5)
+        except socket.error:
+            print "Servidor indisponivel =("
+            self.log(8)
+            exit(2)
 
         # Reajustando o timeout, para falar com o servidor:
         self.sock.settimeout(3.)
 
 
+        # Enviando uma mensagem teste para a outra ponta da conexao, se vier
+        # uma resposta, funcionou a conexao.
+        data = 'Iae vei? Firmeza?'
+
+        # Tenta enviar a mensagem de teste da outra ponta no maximo 3 vezes
+        for i in range(3):
+
+            # Trata timeout
+            try:
+                # Enviando dados e armazenando no log:
+                self.sock.send(data)
+                print "Enviei msg teste para a outra ponta"
+
+                # Aguardando resposta da outra ponta:
+                data = self.sock.recv(1024)
+
+                # Validando a resposta:
+                if data == 'Opa, eh nois!':
+                    print "Conexao efetuada com sucesso"
+                    self.log(6)
+                    break
+
+            except socket.timeout:
+                print "%do estouro de timeout no envio da mensagem teste" % i+1
+                self.log(6)
+
+
+
         # Conexao feita, eh hora de aguardar a entrada do usuario.
         while True:
-
-            # Enviando uma mensagem teste para a outra ponta da conexao, se vier
-            # uma resposta, funcionou a conexao.
-            data = 'Iae vei? Firmeza?'
-
-            # Tenta enviar a mensagem de teste da outra ponta no maximo 3 vezes
-            for i in range(3):
-
-                # Trata timeout
-                try:
-                    # Enviando dados e armazenando no log:
-                    self.sock.send(data)
-                    print "Enviei msg teste para a outra ponta"
-
-                    # Aguardando resposta da outra ponta:
-                    data = self.sock.recv(1024)
-
-                    # Validando a resposta:
-                    if data == 'Opa, eh nois!':
-                        print "Conexao efetuada com sucesso"
-                        self.log(6)
-                        break
-
-                except socket.timeout:
-                    print "Estouro de timeout no envio da mensagem teste"
-                    self.log(6,data)
-
 
             # Recebendo dados do usuario:
             data = raw_input("Digite a expressao matematica: ")
@@ -130,6 +134,6 @@ class Cliente(object):
 
             # Recebendo dados do servidor e atualizando o log
             data = self.sock.recv(1024)
-            print "Recebi %s" % str(data)
+            print "R: %s" % str(data)
             self.log(3,data)
 
